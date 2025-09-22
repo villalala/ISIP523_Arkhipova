80c77bb96a1d6fab5059ac905570ecda7a989cd5
Архипова Василиса <vasyahi117@gmail.com>
2025-09-22 18:26:59 +0300

    эээ рандомное сохранение. ну менюшку начала

--- ISIP523_Arkhipova/Program.cs
+++ ISIP523_Arkhipova/Program.cs
@@ -40,13 +40,47 @@ namespace ISIP523_Arkhipova
         {
             Console.WriteLine($"{Code} | {Name} | {Price}руб | {Quantity}шт. | {Category}");
         }
+        public void vvod()
+        {
+            Console.WriteLine($"Код: ");
+            Console.WriteLine($"Название: ");
+            Console.WriteLine($"Цена: ");
+            Console.WriteLine($"Количества ");
+            Console.WriteLine($"В наличии: ");
+            Console.WriteLine($"Категория: ");
+        }
     }
      
     internal class Program
     {
+        static List<Product> products = new List<Product>();
         static void Main(string[] args)
         {
-            List<Product> products = new List<Product>();
+            
+            while (true)
+            {
+                Console.WriteLine($"Меню");
+                Console.WriteLine($"1. Добавить товар");
+                Console.WriteLine($"2. Удалить товар ");
+                Console.WriteLine($"3. Заказать поставку товара");
+                Console.WriteLine($"4. Продать товар");
+                Console.WriteLine($"5. Поиск товара по коду");
+                Console.WriteLine($"6. Поиск товара по названию");
+                Console.WriteLine($"7. Поиск товара по категории");
+            
+                string n = Console.ReadLine();
+
+                switch(n)
+                {
+                    case "1":
+                        Console.WriteLine("");
+                        break;
+                    case "2":
+                        Console.WriteLine();
+                        break;
+                    case "3":
+                }
+
+            }
+        }
     }
-}
-}
