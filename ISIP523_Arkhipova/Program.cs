8899dbc5aa01f4f089f11954eb36edc6daef0d15
Архипова Василиса <vasyahi117@gmail.com>
2025-09-29 15:43:47 +0300

    структура

--- ISIP523_Arkhipova/Program.cs
+++ ISIP523_Arkhipova/Program.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace ISIP523_Arkhipova
 {
-    internal class Program
+    class analyz
+    {
+        struct Text
+        {
+            public string text;
+            public int word;
+            public string shortt;
+            public int predlozen;
+            public int glasnie;
+            public int soglasn;
+            public string longg;
+            public Dictionary<char, int> povtor;
+        }
+
+        private static List<Text> allStatistics = new List<Text>();
+
+        internal class Program
     {
         static void Main(string[] args)
         {
