055f8c592a2f1b82fd2b019e2fdae086075244ce
Архипова Василиса <vasyahi117@gmail.com>
2025-10-06 15:54:14 +0300

    енум, класс, инфо

--- ISIP523_Arkhipova/Program.cs
+++ ISIP523_Arkhipova/Program.cs
@@ -8,9 +8,47 @@ namespace ISIP523_Arkhipova
 {
     internal class Program
     {
+
+        public enum Janr
+        {
+            Fantasy,
+            Nayka,
+            Detectiv,
+            Horror,
+            Comedy
+        }
+        
+        public class Book
+        {
+            private static int nextId = 1;
+
+        public int id;
+        public string title;
+        public string author;
+        public Janr janr;
+        public int year;
+        public decimal price;
+
+        public Book(string title, string author, Janr janr, int year, double price)
+        {
+            id = nextId++;
+            title = title;
+            author = author;
+            janr = janr;
+            year = year;
+            price = price;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"ID: {id}, Название: {title}, Автор: {author}, Жанр: {janr}, Год: {year}, Цена: {price:C}");
+        }
+
+
+    }
         static void Main(string[] args)
         {
-
-        }
+            
+    }
     }
 }
