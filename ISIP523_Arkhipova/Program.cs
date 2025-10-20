612889f585f4f733c405092f250cba19ee5bf406
Архипова Василиса <vasyahi117@gmail.com>
2025-10-20 15:21:29 +0300

    класс персон, студент, тичер,

--- ISIP523_Arkhipova/Program.cs
+++ ISIP523_Arkhipova/Program.cs
@@ -1,16 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ISIP523_Arkhipova
+namespace ISIP523_Zemdikhanova
 {
-    internal class Program
+    class Person
     {
-        static void Main(string[] args)
-        {
+        private string name;
+        private string surname;
+        private int age;
 
+        public Person(string name, string surname, int age)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine($"Имя: {name}\nФамилия: {surname}\nВозраст: {age}");
+        }
+
+        public string GetFullName()
+        {
+            return $"{name} {surname}";
+        }
+
+        public string GetSurname()
+        {
+            return surname;
+        }
+    }
+
+    class Student : Person
+    {
+        private string group;
+        private int year;
+
+        public Student(string name, string surname, int age, string group, int year)
+            : base(name, surname, age)
+        {
+            this.group = group;
+            this.year = year;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Информация о студенте: ");
+            base.Print();
+            Console.WriteLine($"Группа: {group}\nКурс: {year}");
+        }
+    }
+
+    class Teacher : Person
+    {
+        private string salary;
+        private int experience;
+
+        public Teacher(string name, string surname, int age, string salary, int experience)
+            : base(name, surname, age)
+        {
+            this.salary = salary;
+            this.experience = experience;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Информация о преподавателе: ");
+            base.Print();
+            Console.WriteLine($"Зарплата: {salary}\nСтаж: {experience}");
         }
     }
 }
