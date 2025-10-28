7efd5de323aa3c21d273a225ff82b00d900366fd
Архипова Василиса <vasyahi117@gmail.com>
2025-10-28 22:00:12 +0300

    класс игрока и чутка предметов

--- ISIP523_Arkhipova/Program.cs
+++ ISIP523_Arkhipova/Program.cs
@@ -6,6 +6,58 @@ using System.Threading.Tasks;
 
 namespace ISIP523_Arkhipova
 {
+    class predmet
+    {
+        public string nazvanie;
+        public string tip;
+        public int ataka;
+        public int zaschita;
+
+        public predmet(string nazvanie, string tip, int atk, int def)
+        {
+            this.nazvanie = nazvanie;
+            this.tip = tip;
+            ataka = atk;
+            zaschita = def;
+        }
+    }
+    class player
+    {
+        public int maxHP;
+        public int nowHP;
+        public predmet oruzie;
+        public predmet dospehi;
+        public bool zamorozka;
+        public bool zhiv => nowHP > 0;
+        public int ataka => oruzie.ataka;
+        public int zachita => dospehi.zaschita;
+
+        public player()
+        {
+            maxHP = 100;
+            nowHP = maxHP;
+            oruzie = new predmet("Руки", "оружие", 5, 0);
+            dospehi = new predmet("Кожанные штаны", "доспехи", 0, 2);
+            zamorozka = false;
+        }
+      
+        public void poluchenieUrona(int uron)
+        {
+            nowHP = Math.Max(0, nowHP - uron);
+        }
+
+        public void lechenie (int zdorovie)
+        {
+            nowHP = maxHP;
+        }
+
+        public override string ToString()
+        {
+            return $"Игрок (HP: {nowHP}/{maxHP}, Атака: {ataka}, Защита: {zachita})";
+        }
+    }
+
+    class 
     internal class Program
     {
         static void Main(string[] args)
