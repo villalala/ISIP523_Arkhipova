84e9adc016c43c341a1bf94c8a2d17ca048896c7
Архипова Василиса <vasyahi117@gmail.com>
2025-11-01 12:17:58 +0300

    начало начал

--- ISIP523_Arkhipova/Program.cs
+++ ISIP523_Arkhipova/Program.cs
@@ -10,7 +10,40 @@ namespace ISIP523_Arkhipova
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-        }
-    }
-}
+            int carsServiced = 0; // счётчик обслуженных машин
+            List<(int PartID, int Quantity, int Remaining)> pendingDeliveries = new List<(int PartID, int Quantity, int Remaining)>(); // ожидание деталей
+
+            Console.WriteLine("Добро пожаловать в автосервис!");
+            Console.Write("Введите имя игрока: ");
+            string playername = Console.ReadLine();
+
+            var player = new player
+            {
+                name = playername,
+                balans = 5000,
+                pochinki = 0,
+                TotalFines = 0
+            };
+            CorePR7.Context.player.Add(player);
+            CorePR7.Context.SaveChanges();
+
+            Console.WriteLine($"Игрок {player.name} создан! Баланс: {player.balans}");
+
+            // базовые детали
+            if (!CorePR7.Context.Part.Any())
+            {
+                CorePR7.Context.Part.Add(new Part { name = "Двигатель", PurchasePrice = 1000, RepairPrice = 1500 });
+                CorePR7.Context.Part.Add(new Part { name = "Тормоза", PurchasePrice = 300, RepairPrice = 600 });
+                CorePR7.Context.Part.Add(new Part { name = "Фары", PurchasePrice = 200, RepairPrice = 400 });
+                CorePR7.Context.SaveChanges();
+                Console.WriteLine("Базовые детали добавлены в базу данных.");
+            }
+            else
+            {
+                Console.WriteLine("Детали уже есть в базе данных.");
+            }
+
+            
+
